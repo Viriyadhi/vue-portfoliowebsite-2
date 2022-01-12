3035d68154ad8393dda2336e84fd9d8bfcc36e3c
Viriyadhi <viriyadhitjan@gmail.com>
2022-01-12 18:17:25 +0700

    feat:[VS] static carousels

---
+++ src/components/Carousels.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-app>
+    <v-carousel
+      interval="5000"
+      cycle="true"
+      hide-delimiter-background
+      show-arrows-on-hover
+    >
+      <v-carousel-item
+        v-for="(item, i) in items"
+        :key="i"
+        :src="item.src"
+        reverse-transition="scroll-x-reverse-transition"
+        transition="scroll-x-transition"
+      ></v-carousel-item> </v-carousel
+  ></v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          src: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
+        },
+        {
+          src: "https://cdn.vuetifyjs.com/images/carousel/sky.jpg",
+        },
+        {
+          src: "https://cdn.vuetifyjs.com/images/carousel/bird.jpg",
+        },
+        {
+          src: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,20 @@
 <template>
-  <h1>HelloWorld</h1>
+  <v-app>
+    <carousels />
+    <div class="d-flex flex-row"></div>
+  </v-app>
 </template>
 
 <script>
+import Carousels from "../components/Carousels.vue";
+
 export default {
   name: "Home",
+
+  components: {
+    Carousels,
+  },
 };
 </script>
+
+<style scoped></style>
