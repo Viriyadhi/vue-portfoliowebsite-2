6638695bfb726ddae3155ada2dc1bc968e4482ef
Viriyadhi <viriyadhitjan@gmail.com>
2022-01-20 20:48:11 +0700

    Enhancement:[VS] using grid system for animalList

--- src/components/AnimalList/animalList.vue
+++ src/components/AnimalList/animalList.vue
@@ -1,44 +1,42 @@
 <template>
-  <div class="d-flex flex-column justify-center">
-    <v-card
-      class="rounded-lg d-flex flex-row ma-6 elevation-8"
-      v-for="(item, i) in items"
-      :key="i"
-      :src="item.src"
-    >
-      <v-img
-        width="400"
-        class="rounded-lg rounded-r-0 elevation-8"
-        :src="item.src"
-      >
-      </v-img>
-      <div class="d-flex flex-column">
-        <v-card-text>
-          <h1 class="my-4">Italian, Cafe</h1>
-          <p>
-            {{ description }}
-          </p>
-          <div class="mt-16 pt-12 width">
-            <v-chip-group active-class="primary--text" column>
-              <v-chip v-for="tag in tags" :key="tag">
-                {{ tag }}
-              </v-chip>
-            </v-chip-group>
-          </div>
-        </v-card-text>
+  <div>
+    <v-lazy v-model="isActive">
+      <div class="container">
+        <v-row>
+          <v-col
+            v-for="(item, i) in items"
+            :key="i"
+            :src="item.src"
+            cols="12"
+            sm="4"
+          >
+            <v-hover v-slot="{ hover }">
+              <router-link :to="{ name: 'DetailAnimal' }">
+                <v-card class="elevation-0 rounded-lg">
+                  <v-img class="rounded-lg" :src="item.src" height="350px">
+                    <v-expand-transition>
+                      <div
+                        v-if="hover"
+                        class="d-flex flex-row justsify-space-between transition-fast-in-fast-out darken-2 v-card--reveal white--text"
+                      >
+                        <v-card-title> Lorem, ipsum dolor</v-card-title>
+                      </div>
+                    </v-expand-transition>
+                  </v-img>
+                </v-card>
+              </router-link>
+            </v-hover>
+          </v-col>
+        </v-row>
       </div>
-      <div class="d-flex justify-center align-center mr-16 pr-16">
-        <router-link :to="{ name: 'DetailAnimal' }">
-          <v-btn class="primary rounded"> Pelajari Lebih Lanjut </v-btn>
-        </router-link>
-      </div>
-    </v-card>
+    </v-lazy>
   </div>
 </template>
 
 <script>
 export default {
   data: () => ({
+    isActive: false,
     loading: false,
     selection: 1,
     items: [
@@ -60,14 +58,41 @@ export default {
       {
         src: "https://cdn.vuetifyjs.com/images/carousel/bird.jpg",
       },
+      {
+        src: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
+      },
+      {
+        src: "https://cdn.vuetifyjs.com/images/carousel/sky.jpg",
+      },
+      {
+        src: "https://cdn.vuetifyjs.com/images/carousel/sky.jpg",
+      },
+      {
+        src: "https://cdn.vuetifyjs.com/images/carousel/bird.jpg",
+      },
+      {
+        src: "https://cdn.vuetifyjs.com/images/carousel/bird.jpg",
+      },
+      {
+        src: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
+      },
     ],
     tags: ["Work", "Carnivore", "berbulu"],
     description:
       "Karena spesiesnya yang sangat banyak, kamu bisa menggolongkan hewan menjadi beberapa jenis berdasarkan dengan kelas, cara berjalan, serta cara hewan bernapas. Yuk, cari tahu beragam jenis hewan berdasarkan berbagai golongan di sini!Hewan merupakan salah satu makhluk hidup yang tinggal dan berkembang biak di bumi.Saking banyaknya spesies hewan di dunia, kamu bisa mengklasifikasikannya berdasarkan berbagai jenis golongan.Kamu bisa menggolongkan hewan berdasarkan kelasnya, tempat hidupnya, dan bahkan kulitnya, lo!",
   }),
+
+  methods: {},
 };
 </script>
+
 <style scoped>
+.expanded {
+  margin: auto !important;
+}
+.container {
+  width: 70% !important;
+}
 h1 {
   color: black;
 }
@@ -76,7 +101,12 @@ p {
   width: 70%;
 }
 
-.width {
-  width: 70%;
+.v-card--reveal {
+  align-items: center;
+  bottom: 0;
+  justify-content: center;
+  background-color: rgba(0, 0, 0, 0.25) !important;
+  position: absolute;
+  width: 100% !important;
 }
 </style>
