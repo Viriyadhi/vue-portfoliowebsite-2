d28eddb4b81e666c3097eba26a9c982b01838c5a
Viriyadhi <viriyadhitjan@gmail.com>
2022-01-22 00:13:20 +0700

    Enhancement:[VS] removes the text

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -1,34 +1,11 @@
 <template>
-  <v-footer dark padless>
-    <v-card flat tile color="#f5f8fd" class="lighten-1 text-center">
-      <v-card-text>
-        <v-btn v-for="icon in icons" :key="icon" class="mx-4 black--text" icon>
-          <v-icon size="24px">
-            {{ icon }}
-          </v-icon>
-        </v-btn>
-      </v-card-text>
-
-      <v-card-text class="black--text pt-0">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt,
-        architecto earum repellendus non reiciendis dolores, sequi animi quos
-        nihil modi, eligendi commodi eos placeat cumque? Provident, et dolore
-        accusamus velit laborum ipsam repudiandae? Accusamus ipsam maiores iusto
-        quod voluptas numquam earum! Beatae sed autem, aliquam mollitia quam
-        quibusdam veritatis harum a aliquid. Veritatis aspernatur fugit atque
-        exercitationem repudiandae ab soluta unde nobis quia error dolore nihil
-        magni excepturi odio voluptates, dicta harum sint mollitia laboriosam
-        officia fugiat quasi. Pariatur, culpa! Nostrum nulla, quos magni
-        perspiciatis molestiae eius corporis aut cupiditate commodi rem labore
-        delectus eos consectetur ea nisi repellendus voluptate?
-      </v-card-text>
-
-      <v-divider></v-divider>
-
-      <v-card-text class="black--text">
-        {{ new Date().getFullYear() }} — <strong>Viriyadhi</strong>
-      </v-card-text>
-    </v-card>
+  <v-footer color="white lighten-1">
+    <v-row justify="center" no-gutters>
+      <v-col class="white lighten-2 py-6 text-center black--text" cols="12">
+        © {{ new Date().getFullYear() }} Copyright
+        <strong>Viriyadhi</strong>
+      </v-col>
+    </v-row>
   </v-footer>
 </template>
 <script>
@@ -39,3 +16,8 @@ export default {
   }),
 };
 </script>
+<style scoped>
+.container {
+  width: 100% !important;
+}
+</style>
